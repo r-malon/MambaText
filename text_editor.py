95cdf373ccf778715ca3c2a234da09a9f01d0915
Rafael Malon <divino07rafa@gmail.com>
2019-03-10 19:54:37 -0300

    Fixed fullscreen mode and added modifiable settings

--- text_editor.py
+++ text_editor.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog
+from json import load
 
 def about():
 	messagebox.showinfo('About',
@@ -34,14 +35,26 @@ def replace_text(text):
 	txt.delete("1.0", "end")
 	txt.insert("end", text)
 
+def maximize(event):
+	root.attributes("-fullscreen", True)
+	root.bind("<F11>", minimize)
+
+def minimize(event):
+	root.attributes("-fullscreen", False)
+	root.bind("<F11>", maximize)
+
+with open('settings.json', 'r') as f:
+	settings = load(f)
+
 root = Tk()
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
-root.geometry("720x480+" + str(screen_width//2-360) + "+" + str(screen_height//2-240))
-root.title('My editor')
+root.geometry(settings['size'] + "+" + str(screen_width//2-360) + "+" + str(screen_height//2-240))
+root.title(settings['title'])
 v = StringVar()
-txt = Text(root, background='#2e2e2e', foreground='#c8c8c8', insertbackground='white', 
-	insertwidth=3, insertofftime=200, insertontime=500)
+txt = Text(root, background=settings['background'], foreground=settings['text_color'],
+ insertbackground=settings['insert_color'], insertwidth=settings['insert_width'],
+  insertofftime=200, insertontime=500)
 bottom_bar = Frame(relief=SUNKEN)
 menu = Menu(root)
 text_info = Label(bottom_bar, textvariable=v)
@@ -68,7 +81,7 @@ edit_menu.add_command(label='Copy', command=lambda: root.event_generate("<<Copy>
 edit_menu.add_command(label='Cut', command=lambda: root.event_generate("<<Cut>>"))
 edit_menu.add_command(label='Undo', command=lambda: root.event_generate("<<Undo>>"))
 edit_menu.add_command(label='Redo', command=lambda: root.event_generate("<<Redo>>"))
-#edit_menu.add_command(label='Find', command=txt.search('1.0', 'ola', stopindex='end'))
+edit_menu.add_command(label='Find', command=lambda: txt.search('1.0', 'end', stopindex='end'))
 
 options_menu.add_cascade(label='Change font', menu=font_menu)
 options_menu.add_cascade(label='Change font size', menu=font_size_menu)
@@ -81,14 +94,22 @@ font_menu.add_command(label='Verdana', command=lambda: txt.config(font='Verdana'
 font_menu.add_command(label='Comic Sans', command=lambda: txt.config(font=('Comic Sans MS', 10, 'bold')))
 font_menu.add_command(label='Trebuchet', command=lambda: txt.config(font=('Trebuchet MS', 10)))
 
+font_size_menu.add_command(label='8')
+font_size_menu.add_command(label='10')
+font_size_menu.add_command(label='12')
+font_size_menu.add_command(label='14')
+font_size_menu.add_command(label='18')
+font_size_menu.add_command(label='20')
+font_size_menu.add_command(label='22')
+font_size_menu.add_command(label='24')
+
 help_menu.add_command(label='About', command=about)
 help_menu.add_command(label='Quit', command=root.quit)
 
 root.bind("<Control-s>", saveas)
 root.bind("<Control-o>", open_file)
 root.bind("<Key>", lambda x: v.set(f"Position: {txt.index(INSERT)}; Lines: {int(txt.index('end').split('.')[0]) - 1}; Letters: {len(txt.get('1.0', 'end')) - 1}"))
-root.bind("<F11>", lambda x: root.attributes("-fullscreen", True))
-root.bind("<Escape>", lambda x: root.attributes("-fullscreen", False))
+root.bind("<F11>", maximize)
 
 if __name__ == '__main__':
 	txt.pack(expand=True, fill=BOTH)
