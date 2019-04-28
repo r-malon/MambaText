5d17a4b11b948309cac04a9ce8549be34fad35cb
Rafael Malon <divino07rafa@gmail.com>
2019-04-28 19:45:17 -0300

    Added more options and Lite version.

--- editor_lite.py
+++ editor_lite.py
@@ -1,14 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog
-from tkinter.simpledialog import askstring
-#from tkinter import ttk
-from requests import get
-from requests.exceptions import MissingSchema, InvalidSchema
 from json import load
-from pygame.mixer import music
-from pygame.mixer import init
-from os import listdir
 
 def about():
 	messagebox.showinfo('About', 'Text Editor v0.1\nCreated by R. Malon.')
@@ -37,46 +30,9 @@ def open_file(event=None):
 		replace_text(opened)
 		return True
 
-def scrap_page():
-	link = askstring('Scraping page', 'Enter the link you want to scrap: ')
-	try:
-		response = get(link)
-	except (TclError, MissingSchema, InvalidSchema, ConnectionError):
-		return messagebox.showinfo('Error', 'Invalid link')
-	if txt.get("1.0", "end") != '\n':
-		asked = messagebox.askyesno('Scraping page',
-		 'Do you want to replace the current text?', icon='warning')
-		if asked:
-			replace_text(response.content)
-		else:
-			return False
-	else:
-		replace_text(response.content)
-		return True
-
 def replace_text(text):
 	txt.delete("1.0", "end")
 	txt.insert("end", text)
-
-def find_text(event=None):
-	to_find = askstring('Find', 'Enter what you want to find: ')
-	search_start = '1.0'
-	matches = 0
-	while True:
-		try:
-			length = StringVar()
-			position = txt.search(to_find, search_start, stopindex='end', count=length)
-			txt.tag_add("found", position, f"{position}+{length.get()}c")
-			search_start = f"{position}+{length.get()}c"
-			matches += 1
-		except TclError:
-			messagebox.showinfo('Find', f'{to_find} had {matches} matches')
-			break
-	txt.focus()
-
-def play_song(path):
-	music.load(path)
-	music.play()
 
 def maximize(event):
 	root.attributes("-fullscreen", True)
@@ -100,12 +56,6 @@ def font_config(font=current_font, size=current_font_size, style=current_style):
 	txt.config(font=(font, size, style))
 	current_font, current_font_size, current_style = font, size, style
 
-'''def add_tab():
-	tab = Frame(notebook)
-	notebook.add(tab, text=f'hi {len(tab_list)}')
-	tab_list.append(tab)
-	txt = Text(tab)'''
-
 with open('settings.json', 'r') as f:
 	settings = load(f)
 
@@ -122,16 +72,12 @@ text_info = Label(bottom_bar, textvariable=v)
 txt = Text(root, background=settings['background'], foreground=settings['text_color'],
  insertbackground=settings['insert_color'], insertwidth=settings['insert_width'],
   insertofftime=200, insertontime=500, font=font)
-txt.tag_configure("found", background=settings['found_color'])
-init()
 
 file_menu = Menu(menu, tearoff=0)
 edit_menu = Menu(menu, tearoff=0)
 options_menu = Menu(menu, tearoff=0)
 font_menu = Menu(menu, tearoff=0)
-font_size_menu = Menu(menu, tearoff=0)
 help_menu = Menu(menu, tearoff=0)
-music_menu = Menu(menu, tearoff=0)
 
 menu.add_cascade(label='File', menu=file_menu)
 menu.add_cascade(label='Edit', menu=edit_menu)
@@ -139,7 +85,6 @@ menu.add_cascade(label='Options', menu=options_menu)
 menu.add_separator()
 menu.add_cascade(label='Help', menu=help_menu)
 
-file_menu.add_command(label='New file')#, command=add_tab)
 file_menu.add_command(label='Open file', command=open_file)
 file_menu.add_command(label='Save file', command=saveas)
 
@@ -148,45 +93,25 @@ edit_menu.add_command(label='Copy', command=lambda: txt.event_generate("<<Copy>>
 edit_menu.add_command(label='Cut', command=lambda: txt.event_generate("<<Cut>>"))
 edit_menu.add_command(label='Undo', command=lambda: txt.event_generate("<<Undo>>"))
 edit_menu.add_command(label='Redo', command=lambda: txt.event_generate("<<Redo>>"))
-edit_menu.add_command(label='Find', command=find_text)
 
 options_menu.add_cascade(label='Change font', menu=font_menu)
-options_menu.add_cascade(label='Change font size', menu=font_size_menu)
-options_menu.add_cascade(label='Pick a song', menu=music_menu)
-options_menu.add_command(label='Scrap a webpage', command=scrap_page)
 
 font_menu.add_command(label='Arial', command=lambda: txt.config(font='Arial'))
 font_menu.add_command(label='Times New Roman', command=lambda: txt.config(font='Times'))
 font_menu.add_command(label='Courier', command=lambda: txt.config(font='Courier'))
 font_menu.add_command(label='Georgia', command=lambda: txt.config(font='Georgia'))
 font_menu.add_command(label='Verdana', command=lambda: txt.config(font='Verdana'))
-font_menu.add_command(label='Comic Sans', command=lambda: txt.config(font=('Comic Sans MS', 10, 'bold')))
-font_menu.add_command(label='Trebuchet', command=lambda: txt.config(font=('Trebuchet MS', 10)))
-
-font_size_menu.add_command(label=8, command=lambda: font_config(size=8))
-font_size_menu.add_command(label=10, command=lambda: font_config(size=10))
-font_size_menu.add_command(label=12, command=lambda: font_config(size=12))
-font_size_menu.add_command(label=14, command=lambda: font_config(size=14))
-font_size_menu.add_command(label=18, command=lambda: font_config(size=18))
-font_size_menu.add_command(label=20, command=lambda: font_config(size=20))
-font_size_menu.add_command(label=24, command=lambda: font_config(size=24))
-
-for song in listdir('sound'):
-	music_menu.add_command(label=song, command=lambda: play_song(f'sound/{song}'))
-music_menu.add_command(label='Pause', command=music.pause)
-music_menu.add_command(label='Unpause', command=music.unpause)
 
 help_menu.add_command(label='About', command=about)
 help_menu.add_command(label='Quit', command=root.quit)
 
-root.bind(settings["shortcuts"]["save"], saveas)
-root.bind(settings["shortcuts"]["open"], open_file)
-root.bind(settings["shortcuts"]["find"], find_text)
-root.bind(settings["shortcuts"]["maximize"], maximize)
-root.bind(settings["shortcuts"]["redo"], lambda x: txt.event_generate("<<Redo>>"))
-root.bind(settings["shortcuts"]["menu_hider"], hide_menu)
+root.bind("<Control-s>", saveas)
+root.bind("<Control-o>", open_file)
 root.bind("<Key>", lambda x: v.set(f"Position: {txt.index(INSERT)}; Lines: {int(txt.index('end').split('.')[0]) - 1}; Letters: {len(txt.get('1.0', 'end')) - 1}"))
 root.bind("<Button>", lambda x: v.set(f"Position: {txt.index(INSERT)}; Lines: {int(txt.index('end').split('.')[0]) - 1}; Letters: {len(txt.get('1.0', 'end')) - 1}"))
+root.bind("<F11>", maximize)
+root.bind("<Control-Shift-Z>", lambda x: txt.event_generate("<<Redo>>"))
+root.bind("<Alt-m>", hide_menu)
 
 if __name__ == '__main__':
 	txt.pack(expand=True, fill=BOTH)
